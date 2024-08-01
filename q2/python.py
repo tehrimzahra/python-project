529a3be476ac08d9cb3694b71fb932e790d5048c
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-01 23:27:55 +0500

    python project

---
+++ q2/python.py
@@ -0,0 +1,5 @@
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+total_sum = first_number + second_number
+print(f"The sum of {first_number} and {second_number} is {total_sum}.")
---
+++ q2/q3/python.py
@@ -0,0 +1,4 @@
+
+degrees_fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+degrees_celsius = (degrees_fahrenheit - 32) * 5.0 / 9.0
+print(f"Temperature: {degrees_fahrenheit}F = {degrees_celsius}C")
